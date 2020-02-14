ec0993f62e59c552a0a76ab33f72834a012ee0fa
minorutaso <minorutaso@gmail.com>
2020-02-14 18:48:48 +0900

    k

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,6 @@
+$(function(){
+
+    $().on("keyup",function(){
+     var input = $(".search-input").val();    //フォームの値を取得して変数に代入する
+    });
+  });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
-    def edit
+  def index
+    respond_to do |format|
+      format.html
+      format.json
     end
+  end
   
     def update
       if current_user.update(user_params)
