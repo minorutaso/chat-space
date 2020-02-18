63401ca57c71bcbc429d20444589a2a28058f9e3
minorutaso <minorutaso@gmail.com>
2020-02-18 15:34:49 +0900

    hfgh

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,8 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/minorutaso2.pem.pem'] 
+                  keys: ['~/.ssh/minorutaso2.pembundle install
+                    rake secretenv | grep SECRET_KEY_BASE'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
